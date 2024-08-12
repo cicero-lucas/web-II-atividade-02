df61cf2a2f48979c0e061fc967429a06f8b8fdd2
cicero lucas <cicerolucassilva10@gmail.com>
2024-08-11 23:41:11 -0300

    refactor(controllers): refatorar controllers
    Refatorar os arquivos de controllers para melhorar a estrutura, modularidade e manutenção do código.

--- src/controllers/cadastroPlaca.js
+++ src/controllers/cadastroPlaca.js
@@ -1,29 +1,51 @@
+const CadastroPlaca = require("../models/cadastroPlacaModels");
 const { getOCR } = require("../api/ocr");
 const fs = require("fs");
 
 const cadastroPlaca = async (req, res) => {
   try {
-    console.log(req);
-    const { cidade } = req.body;
+    const {cidade} = req.body;
+
+    // Verifique se o arquivo foi enviado
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "Nenhum arquivo foi enviado.",
+      });
+    }
 
     const filePath = req.file.path;
+    
 
     if (!fs.existsSync(filePath)) {
       throw new Error(`O arquivo no caminho ${filePath} não existe.`);
     }
-    const placaOCR = await getOCR(filePath);
 
-    res
-      .status(200)
-      .json({
-        cidade,
-        placa: placaOCR,
-        data: new Date().toLocaleString("pt-BR", {
-          timeZone: "America/Fortaleza",
-        }),
-      });
+    // Realize a leitura OCR da placa
+    const placaOCR = await getOCR(filePath);
+    console.log(cidade)
+    console.log(placaOCR)
+    // Crie um novo documento no banco de dados com os dados da placa e o caminho da imagem
+    const novaPlaca = new CadastroPlaca({
+      nomeCidade: "crato-ce",
+      placa: placaOCR,
+      caminhoImagem: filePath,
+    });
+
+    await novaPlaca.save();
+
+    // Retorne uma mensagem de sucesso
+    res.status(200).json({
+      success: true,
+      message: "Dados cadastrados com sucesso!",
+      data: novaPlaca,
+    });
   } catch (error) {
-    throw error;
+    res.status(500).json({
+      success: false,
+      message: "Erro ao cadastrar os dados.",
+      error: error.message,
+    });
   }
 };
 
