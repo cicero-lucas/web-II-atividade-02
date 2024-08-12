615d80c911e6de2c62574b462f68ac997ff3f42b
cicero lucas <cicerolucassilva10@gmail.com>
2024-08-11 23:41:57 -0300

    refactor(app): refatorar app.js
    Refatorar app.js para melhorar a organização, modularidade e eficiência do código.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const rotas = require("./routes/route");
+const { conexaoDb } = require("./database/conectDb");
+const cors = require("cors");
 
 const app = express();
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(rotas);
+app.use(express.json());
+app.use(rotas);
+
+conexaoDb();
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
 
 app.listen(process.env.PORT||3000, ()=>{
     console.log(`Server Rodando na Porta: ${process.env.PORT}`)
